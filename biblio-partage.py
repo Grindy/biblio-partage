2556894c1972064e2b07974b961f9558602eabe9
Marc-André Dufour <MAD@nottrue.test>
2025-12-09 19:27:56 -0500

    Ajout de la fonction afficher_livre

--- biblio-partage.py
+++ biblio-partage.py
@@ -1,4 +1,5 @@
 bibliotheque = []
+short = "-" * 50
 
 #ajout d'un livre en dictionnaire dans la liste bibliotheque
 def ajouter_livre(titre, auteur):
@@ -8,8 +9,14 @@ def ajouter_livre(titre, auteur):
 ajouter_livre("Harry Potter", "JK Rowling")
 print(bibliotheque)
 
+# Affichage du contenu de la bibliotheque
 def afficher_livres():
-    pass
+    print(f"{short}\nVoici le contenu de ma biblio :\n")
+    for livre in list(bibliotheque):
+        print("•", livre["titre"], "écrit par", livre["auteur"])
+
+#test affichage du contenu de la bibliotheque
+afficher_livres()
 
 def rechercher_livre(titre):
     
@@ -21,4 +28,6 @@ def rechercher_livre(titre):
             info_livre = f"{livre['titre']} par {livre['auteur']}"
             print(f"{compteur}. {info_livre}")
             compteur += 1
-                
+
+# test rechercher_livre (MAD)
+rechercher_livre("Harry Potter")
