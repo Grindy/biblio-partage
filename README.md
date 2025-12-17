7369e02758af3413a8d34a84785ea5bab3514b04
Mathieu Gosselin <math.gosselin@gmail.com>
2025-12-16 19:47:29 -0500

    doc: Added details in Readme, feat: changed order of functions and presentation.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# biblio-partage
+# 📋 Biblio-partage
+
+Un gestionnaire bibliothèque en Python.
+
+## 🚀 Installation
+
+```bash
+git clone https://github.com/Grindy/biblio-partage.git
+cd biblio-partage
+```
+
+## 📁 Structure du projet
+
+```
+biblio-partage/
+├── README.md
+└── biblio-partage.py   # Main file
+```
+
+## ✨ Fonctionnalités
+
+- 📕 Affiche l'inventaire de la bibliothèque
+- 📕 Permet l'ajout de livres à la bibliothèque
+- 📕 Permet la recherche de livres dans la bibliothèque
+
+## 👨‍💻 Auteurs
+
+Mathieu Gosselin
+Clément Laflamme
+Pascale Mercier
+Marc-André Dufour
+
+## 📄 Licence
+
+Ce projet est sous licence MIT.
--- biblio-partage.py
+++ biblio-partage.py
@@ -3,26 +3,34 @@ bibliotheque = [
     {"titre": "Mémoires d'un Boomer", "auteur": "Marc-André Dufour"},
     {"titre": "Candide", "auteur": "Voltaire"}
     ]
+
 short = "-" * 50
+
+# Affichage du contenu de la bibliotheque
+def afficher_livres():
+    print(f"\nVoici le contenu de ma biblio :\n")
+    for livre in list(bibliotheque):
+        print("•", livre["titre"], "de", livre["auteur"],)
+
+# #test affichage du contenu de la bibliotheque
+afficher_livres()
 
 #ajout d'un livre en dictionnaire dans la liste bibliotheque
 def ajouter_livre(titre, auteur):
     bibliotheque.append({"titre": titre, "auteur":auteur})
+    print(f"\nLe livre {titre} de {auteur} a bien été ajouté à la bibliothèque!")
 
-#test d'ajouter livre
+# #test d'ajouter livre
 ajouter_livre("Harry Potter", "JK Rowling")
-print(bibliotheque)
 
-# Affichage du contenu de la bibliotheque
-def afficher_livres():
-    print(f"{short}\nVoici le contenu de ma biblio :\n")
-    for livre in list(bibliotheque):
-        print("•", livre["titre"], "écrit par", livre["auteur"])
-
-#test affichage du contenu de la bibliotheque
+## test d'affichage après un ajout
 afficher_livres()
 
+print(f"\n{short}")
+
 def rechercher_livre(titre):
+
+    print(f"\nRecherche de: {titre}\n.\n.\n.")
     
     print("\nRésultats trouvés : \n")
 
@@ -33,5 +41,5 @@ def rechercher_livre(titre):
             print(f"{compteur}. {info_livre}")
             compteur += 1
 
-# test rechercher_livre (MAD)
+## test rechercher_livre (MAD)
 rechercher_livre("Harry Potter")
